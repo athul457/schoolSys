4e17a9a0ddb9fdf477796a7cdaeacf1a3e3f9d73
athul457 <athults457@gmail.com>
2026-01-17 18:37:34 +0530

    fixed api url path

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -11,7 +11,8 @@ export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   // Set base URL for axios
-  axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+  // Set base URL for axios
+  axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}/api`;
 
   useEffect(() => {
     const userInfo = localStorage.getItem('userInfo');
